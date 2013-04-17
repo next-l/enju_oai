d8ca5312a06a6e60aab00cdb457b0ebd63c0583a
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-17 11:02:44 +0900

    removed generator

--- lib/generators/enju_oai/setup/setup_generator.rb
+++
@@ -1,8 +0,0 @@
-class EnjuOai::SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def setup
-    inject_into_class 'app/models/manifestation.rb', Manifestation,
-      "  enju_oai\n"
-  end
-end
