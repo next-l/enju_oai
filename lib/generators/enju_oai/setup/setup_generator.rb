8e802e84675af630bedc998d4667e79804435e49
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-08-23 20:36:14 +0900

    removed generator

--- lib/generators/enju_oai/setup/setup_generator.rb
+++
@@ -1,9 +0,0 @@
-class EnjuOai::SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-
-  def copy_setup_files
-    append_to_file 'config/initializers/mime_types.rb',  <<EOS
-Mime::Type.register_alias "text/xml",  :oai
-EOS
-  end
-end
