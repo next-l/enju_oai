39b1ba454e28c5d1fb8ddc8abf50d771890ead95
Masao Takaku <tmasao@acm.org>
2015-08-18 16:34:10 +0900

    add spec for GetRecord with junii2. cf. next-l/enju_leaf#619

--- spec/views/manifestations/show.oai.builder_spec.rb
+++ spec/views/manifestations/show.oai.builder_spec.rb
@@ -15,4 +15,18 @@ describe "manifestations/show.oai.builder" do
       rendered.should match /<dc:title>manifestation_title/
     end
   end
+
+  describe "When metadataPrefix is 'junii2'" do
+    before(:each) do
+      assign(:manifestation, FactoryGirl.create(:manifestation))
+      assign(:oai, metadataPrefix: 'junii2')
+    end
+
+    it "renders the XML template" do
+      render
+      rendered.should match /<metadata\b/
+      rendered.should match /<junii2\b/
+      rendered.should match /<title>manifestation_title/
+    end
+  end
 end
