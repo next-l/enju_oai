c544307db5eef147daf0eca37e595d0ca7e2b28c
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-12 17:13:27 +0900

    update Gemfile

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include EnjuLeaf::Controller
-  include EnjuBiblio::Controller
   include EnjuLibrary::Controller
+  include EnjuBiblio::Controller
   include EnjuSubject::Controller
   after_action :verify_authorized
 
