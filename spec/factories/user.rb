3d52bd91ea0156c3a9c4326925a5ccf62ac672e2
Kosuke Tanabe <nabeta@fastmail.fm>
2019-04-01 02:41:38 +0900

    update vcr file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     f.sequence(:username){|n| "admin_#{n}"}
     f.sequence(:email){|n| "admin_#{n}@example.jp"}
     f.role {Role.find_by_name('Administrator')}
-    f.password 'adminpassword'
-    f.password_confirmation 'adminpassword'
+    f.password { 'adminpassword' }
+    f.password_confirmation { 'adminpassword' }
     f.user_group {UserGroup.first}
-    f.required_role {Role.find_by_name('User')}
+    f.required_role {Role.find_by(name: 'User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
   end
 
@@ -14,10 +14,10 @@ FactoryBot.define do
     f.sequence(:username){|n| "librarian_#{n}"}
     f.sequence(:email){|n| "librarian_#{n}@example.jp"}
     f.role {Role.find_by_name('Librarian')}
-    f.password 'librarianpassword'
-    f.password_confirmation 'librarianpassword'
+    f.password { 'librarianpassword' }
+    f.password_confirmation { 'librarianpassword' }
     f.user_group {UserGroup.first}
-    f.required_role {Role.find_by_name('User')}
+    f.required_role {Role.find_by(name: 'User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
   end
 
@@ -25,10 +25,10 @@ FactoryBot.define do
     f.sequence(:username){|n| "user_#{n}"}
     f.sequence(:email){|n| "user_#{n}@example.jp"}
     f.role {Role.find_by_name('User')}
-    f.password 'userpassword'
-    f.password_confirmation 'userpassword'
+    f.password { 'userpassword' }
+    f.password_confirmation { 'userpassword' }
     f.user_group {UserGroup.first}
-    f.required_role {Role.find_by_name('User')}
+    f.required_role {Role.find_by(name: 'User')}
     f.sequence(:user_number){|n| "user_number_#{n}"}
   end
 
