f5426916def95a70fb879dd7548f1c2f9e52920e
Kosuke Tanabe <nabeta@fastmail.fm>
2019-07-14 00:54:11 +0900

    update bin files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+SimpleCov.start 'rails'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
